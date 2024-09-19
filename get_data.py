606d91002805c98a9a08897f4c97674882ff4f31
Nikolai-Grytvik-Borbe <nikolaiborbe@gmail.com>
2024-09-19 16:44:33 +0200

    added get_data functino

--- get_data.py
+++ get_data.py
@@ -1,12 +1,19 @@
 import numpy as np
 
-def get_data(): 
+def get_data() -> list[list]:
+    """
+    Returns formatted data
 
-    data = np.loadtxt("ResultatKollisjon1.txt", delimiter=",")
-    print(data)
+    Returns:
+        list: -> [time], [puck1], [puck2]
+    """
 
+    data  = np.loadtxt("ResultatKollisjon1.csv", delimiter=";")
+    time  = []
     puck1 = []
     puck2 = []
-    return [puck1, puck2]
 
-get_data()
+    time  = data[:,0]
+    puck1 = [data[:,1], data[:,2]]
+    puck2 = [data[:,3], data[:,4]]
+    return [time, puck1, puck2]
