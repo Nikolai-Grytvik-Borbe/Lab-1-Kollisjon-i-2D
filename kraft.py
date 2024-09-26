58777733ab5d2d7824e9de71491d1a8be2e88960
Karl-ArneOpkvitne <karlarnesand@gmail.com>
2024-09-26 15:51:45 +0200

    rask endring

--- kraft.py
+++ kraft.py
@@ -4,8 +4,8 @@ from constants import MASSE1, MASSE2
 posisjon1 = get_puck1()
 posisjon2 = get_puck2()
 
-fart1, akselerasjon1 = beregning(posisjon1, "1")
-fart2, akselerasjon2 = beregning(posisjon2, "2")
+fart1, akselerasjon1 = beregning(posisjon1)
+fart2, akselerasjon2 = beregning(posisjon2)
 
 
 def kraft_liste(akselerasjon, masse):
